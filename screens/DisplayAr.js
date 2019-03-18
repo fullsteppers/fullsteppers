d54ffb336772818e5cfdb72b17c72f1ff53fd5a4
Zachary Marszale <zmarszal@ameritech.net>
2019-03-18 14:32:59 -0500

    working with api key

--- screens/DisplayAr.js
+++ screens/DisplayAr.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { VIRO_API } from './../secrets'
 import { ViroARSceneNavigator} from 'react-viro'
 var InitialARScene = require("./../js/HelloWorldSceneAR");
 
@@ -7,7 +8,7 @@ export default class DisplayAr extends React.Component {
     const { dance, song } = this.props
     return (
       <ViroARSceneNavigator
-        apiKey="629A9C94-12FB-4AE8-8ACF-BEF10AAF6CEB"
+        apiKey={VIRO_API}
         initialScene={{ scene: InitialARScene }}
         viroAppProps={{dance, song}}
       />
