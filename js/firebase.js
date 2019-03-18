4e6f8c888527542a39b983889c80b95a06088ad3
Rose Graner <rosegraner@gmail.com>
2019-03-18 14:06:39 -0500

    secrets initialized

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,8 @@
-// import * as firebase from "firebase";
+import "../secrets.js";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA6OHgv5x9_goudqMxyyT4odBxGtGed4Gc",
-//   authDomain: "footsteppers-proofofconcept.firebaseapp.com",
-//   databaseURL: "https://footsteppers-proofofconcept.firebaseio.com/",
-//   storageBucket: "footsteppers-proofofconcept.appspot.com"
-// };
-
-// firebase.initializeApp(firebaseConfig);
-// const database = firebase.database();
-
-// export function makeRef(path) {
-//   return database.ref(path);
-// }
 import * as firebase from "firebase";
 var config = {
-  apiKey: "AIzaSyDXFmxBJCd9G3npQ3tuVdseewSQnxCsiZs",
+  apiKey: process.env.GOOGLE_API,
   authDomain: "new-project-for-web.firebaseapp.com",
   databaseURL: "https://new-project-for-web.firebaseio.com",
   projectId: "new-project-for-web",
