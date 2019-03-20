01d49dc176e7764c4bbadcdf1d331a77f7581e5b
Rose Graner <rosegraner@gmail.com>
2019-03-20 13:58:44 -0500

    beginning to style displayar end

--- screens/DisplayAr.js
+++ screens/DisplayAr.js
@@ -1,77 +1,133 @@
-import React from 'react'
-require('./../secrets')
-import { ViroARSceneNavigator} from 'react-viro'
+import React from "react";
+import { Actions } from "react-native-router-flux";
+require("./../secrets");
+import { ViroARSceneNavigator } from "react-viro";
 var InitialARScene = require("./../js/HelloWorldSceneAR");
-import timer from 'react-native-timer'
-import {  View, Text, Button } from 'react-native'
-import { Actions } from 'react-native-router-flux'
+import timer from "react-native-timer";
+import getTheme from "../native-base-theme/components";
+import {
+  Container,
+  Card,
+  Content,
+  CardItem,
+  Text,
+  StyleProvider,
+  Body,
+  Button
+} from "native-base";
 
 export default class DisplayAr extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       go: true
-    }
-    this.startOver = this.startOver.bind(this)
-    this.newTrack = this.newTrack.bind(this)
-    this.newDance = this.newDance.bind(this)
-    this.goHome = this.goHome.bind(this)
+    };
+    this.startOver = this.startOver.bind(this);
+    this.newTrack = this.newTrack.bind(this);
+    this.newDance = this.newDance.bind(this);
+    this.goHome = this.goHome.bind(this);
   }
-  componentDidMount(){
-    timer.setTimeout('start', () => {
-      this.setState({go: false})
-    }, 60000);
+  componentDidMount() {
+    timer.setTimeout(
+      "start",
+      () => {
+        this.setState({ go: false });
+      },
+      60000
+    );
   }
 
-  componentWillMount(){
-    timer.clearTimeout(this)
+  componentWillMount() {
+    timer.clearTimeout(this);
   }
 
   startOver() {
-    timer.setTimeout('start', () => {
-      this.setState({go: false})
-    }, 60000);
-    this.setState({go: true})
+    timer.setTimeout(
+      "start",
+      () => {
+        this.setState({ go: false });
+      },
+      60000
+    );
+    this.setState({ go: true });
   }
 
-  newTrack(){
-    Actions.SelectSong({dance: this.props.dance})
+  newTrack() {
+    Actions.SelectSong({ dance: this.props.dance });
   }
-  newDance(){
-    Actions.SelectDance()
+  newDance() {
+    Actions.SelectDance();
   }
-  goHome(){
-    Actions.Home()
+  goHome() {
+    Actions.Home();
   }
 
   render() {
-    const { dance, song } = this.props
-    if(this.state.go) {
-      return (
-          <ViroARSceneNavigator
-            apiKey={process.env.VIRO_API}
-            initialScene={{ scene: InitialARScene }}
-            viroAppProps={{dance, song}}
-          />
-      )
-    } else {
-      return (
-        <View>
-          <Text>You just did the {dance} to the song: {song}</Text>
-          <Button
-          title='Start Over'
-          onPress={this.startOver} />
-          <Button
-          title='Pick a New Track'
-          onPress={this.newTrack} />
-          <Button
-          title='Pick a New Dance'
-          onPress={this.newDance} />
-          <Button
-          title='Home'
-          onPress={this.goHome} />
-        </View>
-      )
-    }
+    const { dance, song } = this.props;
+    // if (this.state.go) {
+    //   return (
+    //     <ViroARSceneNavigator
+    //       apiKey={process.env.VIRO_API}
+    //       initialScene={{ scene: InitialARScene }}
+    //       viroAppProps={{ dance, song }}
+    //     />
+    //   );
+    // } else {
+    return (
+      <StyleProvider style={getTheme()}>
+        <Container>
+          <Card>
+            <CardItem>
+              <Body>
+                <Text>
+                  You just did the {dance} to the song: {song}
+                </Text>
+                <Text>Start Over</Text>
+                <Button title="Start Over" />
+                <Text>end of card 1</Text>
+              </Body>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Button title="Start Over" onPress={this.startOver}>
+                  <Text>end of card 2</Text>
+                </Button>
+              </Body>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>Start Over</Text>
+                <Button title="Start Over" />
+                <Text>end of card 3</Text>
+              </Body>
+            </CardItem>
+          </Card>
+        </Container>
+      </StyleProvider>
+
+      // <Content>
+      //   <Text>
+      //     You just did the {dance} to the song: {song}
+      //   </Text>
+      //   <Button title="Start Over" onPress={this.startOver} />
+
+      //   <Button title="Start Over" onPress={this.startOver}>
+      //     <Text>Start Over</Text>
+      //   </Button>
+
+      //   <Button title="Pick a New Track" onPress={this.newTrack}>
+      //     <Text>Pick a New Track</Text>
+      //   </Button>
+
+      //   <Button title="Pick a New Dance" onPress={this.newDance}>
+      //     <Text>Pick a New Dance</Text>
+      //   </Button>
+
+      //   <Button title="Home" onPress={this.goHome}>
+      //     <Text>Home</Text>
+      //   </Button>
+      // </Content>
+    );
+    // }
   }
 }
