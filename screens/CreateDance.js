57b3e31dc6442c6d0ccbb680013525e17a89ca38
Rose Graner <rosegraner@gmail.com>
2019-03-21 13:18:00 -0500

    menu assembled and functions started.

--- screens/CreateDance.js
+++ screens/CreateDance.js
@@ -1,116 +1,116 @@
-import React from 'react'
-import {  View, Text, Image, Animated, TouchableHighlight } from 'react-native'
-import Arrow from 'react-native-arrow'
+import React from "react";
+import { View, Image, Animated, TouchableHighlight } from "react-native";
+import { Col, Row, Grid, Button, Text } from "native-base";
+import CreateDanceMenu from "../js/CreateDanceMenu";
 
 export default class createDance extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       leftX: 100,
       leftY: 170,
       rightX: 145,
       rightY: 170,
-      currentFoot: 'right'
-    }
-    this.moveAnimationLeft = new Animated.ValueXY({ x: this.state.leftX, y: this.state.leftY })
-    this.moveAnimationRight = new Animated.ValueXY({ x: this.state.rightX, y: this.state.rightY })
-    this.move = this.move.bind(this)
+      currentFoot: "right",
+      leftMoves: [],
+      rightMoves: [],
+      currentMove: ""
+    };
+    this.moveAnimationLeft = new Animated.ValueXY({
+      x: this.state.leftX,
+      y: this.state.leftY
+    });
+    this.moveAnimationRight = new Animated.ValueXY({
+      x: this.state.rightX,
+      y: this.state.rightY
+    });
+    this.move = this.move.bind(this);
+    this.switchFoot = this.switchFoot.bind(this);
   }
 
-  move(x, y){
-    let newX = this.state.rightX
-    let newY = this.state.rightY
-    if(y === 'forward'){
-      newY -= 100
-      this.setState({[`${this.state.currentFoot}Y`]: newY})
-     } else if (y === 'backward') {
-      newY += 100
-      this.setState({[`${this.state.currentFoot}Y`]: newY})
-     }
-     if(x === 'right') {
-      newX += 60
-      this.setState({[`${this.state.currentFoot}X`]: newX})
-     } else if(x === 'left') {
-      newX -= 60
-      this.setState({[`${this.state.currentFoot}X`]: newX})
-     }
+  move(x, y) {
+    let newX =
+      this.state.currentFoot === "right" ? this.state.rightX : this.state.leftX;
+    let newY =
+      this.state.currentFoot === "right" ? this.state.rightY : this.state.leftY;
+    if (y === "forward") {
+      newY -= 100;
+      this.setState({ [`${this.state.currentFoot}Y`]: newY });
+    } else if (y === "backward") {
+      newY += 100;
+      this.setState({ [`${this.state.currentFoot}Y`]: newY });
+    }
+    if (x === "right") {
+      newX += 60;
+      this.setState({ [`${this.state.currentFoot}X`]: newX });
+    } else if (x === "left") {
+      newX -= 60;
+      this.setState({ [`${this.state.currentFoot}X`]: newX });
+    }
+    if (this.state.currentFoot === "right") {
+      Animated.spring(this.moveAnimationRight, {
+        toValue: { x: newX, y: newY }
+      }).start();
+    } else {
+      Animated.spring(this.moveAnimationLeft, {
+        toValue: { x: newX, y: newY }
+      }).start();
+    }
+  }
 
-    Animated.spring(this.moveAnimationRight, {
-      toValue: { x: newX, y: newY},
-    }).start()
+  switchFoot() {
+    this.state.currentFoot === "right"
+      ? this.setState({ currentFoot: "left" })
+      : this.setState({ currentFoot: "right" });
   }
 
   render() {
     return (
-      <View style={{flex: 1, flexDirection:'column'}}>
-        <View style={{flex: 1, flexDirection:'row'}}>
-          <Animated.View style={[this.moveAnimationLeft.getLayout()]}>
-            <Image
-            source={require('./../js/res/leftfoot.png')}
-            style={{width: 66, height: 160}}/>
-          </Animated.View>
-          <Animated.View style={[this.moveAnimationRight.getLayout()]}>
-            <Image
-            source={require('./../js/res/rightfoot.png')}
-            style={{width: 66, height: 160}}/>
-          </Animated.View>
+      <View style={{ flex: 1, flexDirection: "column" }}>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <Grid>
+            <Row size={4}>
+              <Animated.View
+                style={[
+                  { backgroundColor: "transparent" },
+                  this.moveAnimationLeft.getLayout()
+                ]}
+              >
+                <Image
+                  source={require("./../js/res/leftfoot.png")}
+                  style={{ width: 66, height: 160 }}
+                />
+              </Animated.View>
+              <Animated.View style={[this.moveAnimationRight.getLayout()]}>
+                <Image
+                  source={require("./../js/res/rightfoot.png")}
+                  style={{ width: 66, height: 160 }}
+                />
+              </Animated.View>
+            </Row>
+            <Row size={1}>
+              <Col>
+                <Button>
+                  <Text>Save Move</Text>
+                </Button>
+                <Button vertical>
+                  <Text>Submit Dance</Text>
+                </Button>
+              </Col>
+              <Col>
+                <CreateDanceMenu move={this.move} />
+              </Col>
+              <Col>
+                <Text note>Current foot: {this.state.currentFoot}</Text>
+                <Button onPress={this.switchFoot}>
+                  <Text>Switch</Text>
+                </Button>
+              </Col>
+            </Row>
+          </Grid>
         </View>
-        <View style={{display: 'flex', flexDirection: 'column'}}>
-          <View>
-            <TouchableHighlight style={{height: 15, width:15}} onPress={() => this.move('left', 'forward')}>
-              <Image
-              source={require('./../js/res/back_arrow.png')}
-              style={{width: 15, height: 15, transform: [{ rotate: '45deg'}]}}/>
-            </TouchableHighlight>
-
-            <TouchableHighlight style={{height: 15, width:15}} onPress={() => this.move('stay', 'forward')}>
-              <Image
-              source={require('./../js/res/back_arrow.png')}
-              style={{width: 15, height: 15, transform: [{ rotate: '90deg'}]}}/>
-            </TouchableHighlight>
-
-            <TouchableHighlight style={{height: 15, width:15}} onPress={() => this.move('right', 'forward')}>
-              <Image
-              source={require('./../js/res/back_arrow.png')}
-              style={{width: 15, height: 15, transform: [{ rotate: '135deg'}]}}/>
-            </TouchableHighlight>
-          </View>
-
-            <TouchableHighlight style={{height: 15, width:15}} onPress={() => this.move('left', 'stay')}>
-            <Image
-            source={require('./../js/res/back_arrow.png')}
-            style={{width: 15, height: 15}}/>
-            </TouchableHighlight>
-
-
-            <TouchableHighlight style={{height: 15, width:15}} onPress={() => this.move('right', 'stay')}>
-              <Image
-              source={require('./../js/res/back_arrow.png')}
-              style={{width: 15, height: 15, transform: [{ rotate: '180deg'}]}}/>
-            </TouchableHighlight>
-
-            <TouchableHighlight style={{height: 15, width:15}} onPress={() => this.move('stay', 'backward')}>
-              <Image
-              source={require('./../js/res/back_arrow.png')}
-              style={{width: 15, height: 15, transform: [{ rotate: '270deg'}]}}/>
-            </TouchableHighlight>
-
-
-            <TouchableHighlight style={{height: 15, width:15}} onPress={() => this.move('right', 'backward')}>
-              <Image
-              source={require('./../js/res/back_arrow.png')}
-              style={{width: 15, height: 15, transform: [{ rotate: '225deg'}]}}/>
-            </TouchableHighlight>
-
-
-            <TouchableHighlight style={{height: 15, width:15}} onPress={() => this.move('left', 'backward')}>
-              <Image
-              source={require('./../js/res/back_arrow.png')}
-              style={{width: 15, height: 15, transform: [{ rotate: '315deg'}]}}/>
-            </TouchableHighlight>
-
-        </View>
+        <View />
       </View>
-    )
+    );
   }
 }
