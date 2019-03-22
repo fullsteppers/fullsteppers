b9f6d88f4e98931140eecccf2356df640a8926a3
Rose Graner <rosegraner@gmail.com>
2019-03-22 15:50:02 -0500

    create dance runs and anon auth works and real db is used

--- js/firebase.js
+++ js/firebase.js
@@ -1,24 +1,28 @@
 import * as firebase from "firebase";
+require("./../secrets");
 
-// var config = {
-//   apiKey: process.env.GOOGLE_API,
-//   authDomain: "new-project-for-web.firebaseapp.com",
-//   databaseURL: "https://new-project-for-web.firebaseio.com",
-//   projectId: "new-project-for-web",
-//   storageBucket: "",
-//   messagingSenderId: "608301246833"
-// };
 var config = {
-  apiKey: "AIzaSyB2a-RqRtVe8Gl4ow4nFOsvoSdPMwPA_Bs",
-  authDomain: "test-web-db-557e4.firebaseapp.com",
-  databaseURL: "https://test-web-db-557e4.firebaseio.com",
-  projectId: "test-web-db-557e4",
-  storageBucket: "test-web-db-557e4.appspot.com",
-  messagingSenderId: "116585550981"
+  apiKey: process.env.GOOGLE_API,
+  authDomain: "new-project-for-web.firebaseapp.com",
+  databaseURL: "https://new-project-for-web.firebaseio.com",
+  projectId: "new-project-for-web",
+  storageBucket: "",
+  messagingSenderId: "608301246833"
 };
+
 var app = firebase.initializeApp(config);
 const database = firebase.database();
 
 export function makeRef(path) {
   return database.ref(path);
 }
+
+//FOR TESTING PURPOSES:
+// var config = {
+//   apiKey: "AIzaSyB2a-RqRtVe8Gl4ow4nFOsvoSdPMwPA_Bs",
+//   authDomain: "test-web-db-557e4.firebaseapp.com",
+//   databaseURL: "https://test-web-db-557e4.firebaseio.com",
+//   projectId: "test-web-db-557e4",
+//   storageBucket: "test-web-db-557e4.appspot.com",
+//   messagingSenderId: "116585550981"
+// };
